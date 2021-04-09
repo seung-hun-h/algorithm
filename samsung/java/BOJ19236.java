ee25ae7ae4200027d2c5da451fafe24f459a21bf
seung-h <dau4723@gmail.com>
2021-04-09 16:29:19 +0900

    2021-04-09

---
+++ samsung/java/BOJ19236.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ19236 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
