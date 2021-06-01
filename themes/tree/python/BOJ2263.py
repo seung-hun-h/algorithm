30301350ea7b2de0b1dea77623d48ec315e46b8e
seung-h <dau4723@gmail.com>
2021-06-01 14:05:37 +0900

    Optimize

--- themes/tree/python/BOJ2263.py
+++ themes/tree/python/BOJ2263.py
@@ -6,22 +6,25 @@ N = int(readline())
 inorder = list(map(int, readline().split()))
 postorder = list(map(int, readline().split()))
 
+p_idx = len(postorder) - 1
 def solve():
     index = dict()
+    stack = []
     for i, n in enumerate(inorder):
         index[n] = i
 
-    dfs(0, N-1, 0, N-1, index)
+    dfs(0, N-1, index, stack)
+    stack.reverse()
+    print(*stack)
 
-def dfs(in_s, in_e, post_s, post_e, index):
-    if (in_s > in_e) or (post_s > post_e):
+def dfs(in_s, in_e, index, stack):
+    global p_idx
+    if in_s > in_e:
         return
-    root = postorder[post_e]
-    print(root, end=' ')
-    left = index[root] - in_s # 왼쪽 서브 트리의 크기
-    right = in_e - index[root] # 오른쪽 서브 트리의 크기
+    root = postorder[p_idx]
+    p_idx -= 1
 
-    dfs(in_s, in_s+left-1, post_s, post_s+left-1, index) # 왼쪽 서브트리    
-    dfs(in_e-right+1, in_e, post_e-right, post_e-1, index) # 오른쪽 서브 트리
-
+    dfs(index[root]+1, in_e, index, stack) # 오른쪽 서브 트리
+    dfs(in_s, index[root]-1, index, stack) # 왼쪽 서브트리    
+    stack.append(root)
 solve()
