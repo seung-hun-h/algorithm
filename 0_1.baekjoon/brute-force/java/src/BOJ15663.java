1fb317b0eb3cb4d80c568ec5ef6a800ffe9e731a
seunghun <dau4723@gmail.com>
2022-01-07 09:58:49 +0900

    2022-01-07 BOJ bruteforce N과 M시리즈

---
+++ 0_1.baekjoon/brute-force/java/src/BOJ15663.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ15663 {
+
+    private static int N, M;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        Map<Integer, Integer> count = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            count.putIfAbsent(key, 0);
+            count.compute(key, (k, v) -> v + 1);
+        }
+
+        arr  = new int[count.size()];
+
+        int i = 0;
+        for (int num : count.keySet())  {
+            arr[i++] = num;
+        }
+
+        Arrays.sort(arr);
+        N = arr.length;
+        dfs(new int[M], 0, count);
+
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int[] current, int depth, Map<Integer, Integer> count) {
+        if (depth == M) {
+            for (int num : current) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (count.get(arr[i]) > 0) {
+                count.compute(arr[i], (k, v) -> v - 1);
+                current[depth] = arr[i];
+                dfs(current, depth + 1, count);
+                count.compute(arr[i], (k, v) -> v + 1);
+            }
+        }
+    }
+}
---
+++ 0_1.baekjoon/brute-force/java/src/BOJ15666.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ15666 {
+
+    private static int N, M;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        arr  = new int[set.size()];
+
+        int i = 0;
+        for (int num : set) {
+            arr[i++] = num;
+        }
+
+        Arrays.sort(arr);
+        N = arr.length;
+        dfs(new int[M], 0, 0);
+
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int[] current, int depth, int idx) {
+        if (depth == M) {
+            for (int num : current) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            current[depth] = arr[i];
+            dfs(current, depth + 1, i);
+        }
+    }
+}
